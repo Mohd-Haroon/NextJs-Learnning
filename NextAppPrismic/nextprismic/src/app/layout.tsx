40a287468d61aaa52a9d2ad33c8f329e939e13c1
Haroon Qureshi <haroonqureshi@Haroons-MacBook-Air.local>
2025-03-03 12:53:04 +0530

    added font and clear boilerplate

--- NextAppPrismic/nextprismic/src/app/layout.tsx
+++ NextAppPrismic/nextprismic/src/app/layout.tsx
@@ -1,16 +1,19 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Nunito,Nunito_Sans } from "next/font/google";
 import "./globals.css";
+import clsx from "clsx";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const nunito = Nunito({
+  subsets: ['latin'],
+  variable: '--font-nunito',
+  display: 'swap',
+})
+const nunito_sans = Nunito_Sans({
+  subsets: ['latin'],
+  variable: '--font-nunito-sans',
+  display: 'swap',
+})
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,10 +26,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className={clsx(nunito.variable,nunito_sans.variable)}>
+      <body>
         {children}
       </body>
     </html>
--- firstapp/app/users/page.tsx
+++ firstapp/app/users/page.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
+interface User {
+    id : number;
+    name : String;
+}
+
 const UserPage = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
-    const data = await res.json();
+    const users: User[] = await res.json();
     return (
-        <div>UserPage</div>
+        <>
+            <h1>Users</h1>
+            <ul>
+                {users.map(el => <li key={el.id}>{el.id}-{el.name}</li>)}
+            </ul>
+        </>
     )
 }
 
