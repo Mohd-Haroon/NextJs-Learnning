46251a976d307585a9cf34bf5463ccb413721796
Haroon Qureshi <haroonqureshi@Haroons-MacBook-Air.local>
2025-03-05 11:47:14 +0530

    Homepage added

--- NextAppPrismic/nextprismic/src/app/page.tsx
+++ NextAppPrismic/nextprismic/src/app/page.tsx
@@ -1,9 +1,36 @@
-import Image from "next/image";
+import { Metadata } from "next";
+import { isFilled, asImageSrc } from "@prismicio/client";
+import { SliceZone } from "@prismicio/react";
 
-export default function Home() {
-  return (
-    <main className="">
-      <h1 className="font-display text-5xl">hello World</h1>
-    </main>
-  );
+import { createClient } from "@/prismicio";
+// import { components } from "@/slices";
+
+export default async function Page() {
+  const client = createClient();
+  const page = await client.getSingle("homepage");
+
+  return <div className="text-red-500">Sweet Home !!!</div>
+
+  // return <SliceZone slices={page.data.slices} components={components} />;
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const page = await client.getSingle("homepage");
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+    // openGraph: {
+    //   title: isFilled.keyText(page.data.meta_title)
+    //     ? page.data.meta_title
+    //     : undefined,
+    //   description: isFilled.keyText(page.data.meta_description)
+    //     ? page.data.meta_description
+    //     : undefined,
+    //   images: isFilled.image(page.data.meta_image)
+    //     ? [asImageSrc(page.data.meta_image)]
+    //     : undefined,
+    // },
+  };
 }
