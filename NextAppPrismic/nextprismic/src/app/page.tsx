537b3574cf76f9a92b2f341acd3d7fae100729e7
Haroon Qureshi <haroonqureshi@Haroons-MacBook-Air.local>
2025-04-28 10:37:13 +0530

    feat create hero slice & helper componets

--- NextAppPrismic/nextprismic/src/app/page.tsx
+++ NextAppPrismic/nextprismic/src/app/page.tsx
@@ -3,15 +3,13 @@ import { isFilled, asImageSrc } from "@prismicio/client";
 import { SliceZone } from "@prismicio/react";
 
 import { createClient } from "@/prismicio";
-// import { components } from "@/slices";
+import { components } from "@/slices";
 
 export default async function Page() {
   const client = createClient();
   const page = await client.getSingle("homepage");
 
-  return <div className="text-red-500">Sweet Home !!!</div>
-
-  // return <SliceZone slices={page.data.slices} components={components} />;
+  return <SliceZone slices={page.data.slices} components={components} />;
 }
 
 export async function generateMetadata(): Promise<Metadata> {
@@ -21,16 +19,16 @@ export async function generateMetadata(): Promise<Metadata> {
   return {
     title: page.data.meta_title,
     description: page.data.meta_description,
-    // openGraph: {
-    //   title: isFilled.keyText(page.data.meta_title)
-    //     ? page.data.meta_title
-    //     : undefined,
-    //   description: isFilled.keyText(page.data.meta_description)
-    //     ? page.data.meta_description
-    //     : undefined,
-    //   images: isFilled.image(page.data.meta_image)
-    //     ? [asImageSrc(page.data.meta_image)]
-    //     : undefined,
-    // },
+    openGraph: {
+      title: isFilled.keyText(page.data.meta_title)
+        ? page.data.meta_title
+        : undefined,
+      description: isFilled.keyText(page.data.meta_description)
+        ? page.data.meta_description
+        : undefined,
+      images: isFilled.image(page.data.meta_image)
+        ? [asImageSrc(page.data.meta_image)]
+        : undefined,
+    },
   };
 }
